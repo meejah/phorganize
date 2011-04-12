754d8241e56a2761700a979c35ba3f11569300a2
mike warren <meejah@gmail.com>
2011-04-11 21:35:40 -0600

    rotate video by 90 degress

---
+++ scripts/rotate-video.py
@@ -0,0 +1,24 @@
+#!/usr/bine/env python
+
+##
+## using "transcode" this will stabalize a shaky video
+## see http://public.hronopik.de/vid.stab/features.php?lang=en
+##
+## options: input-file and output-file
+##
+
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print "usage: %s input output" % sys.argv
+    sys.exit(-1)
+
+infname = sys.argv[1]
+outfname = sys.argv[2]
+
+print "rescaling video %s writing to %s" % (infname, outfname)
+
+cmd = 'mencoder "%s" -oac copy -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -vf rotate=1 -o "%s"' % (infname, outfname)
+print cmd
+os.system(cmd)
