1df7e0dc3830ee44fbeee3770ede0cda4f27dc73
mike warren <meejah@gmail.com>
2011-07-25 17:19:07 -0600

    Rework a little so we get all the names etc. and then upload in one go

--- scripts/flickr_upload.py
+++ scripts/flickr_upload.py
@@ -20,7 +20,7 @@ if len(sys.argv) < 2:
     print "usage: %s JPEG [JPEG]" % sys.argv[0]
     sys.exit(-1)
 
-token = fapi.getToken(browser="lynx",perms="delete")
+token = fapi.getToken(browser="lynx")##,perms="delete")
 mikeapi = MikeFlickr(fapi,flickrAPIKey,token)
 
 ## methods
@@ -52,12 +52,20 @@ print "found",len(toupload),"images to upload."
 
 tmpdir = tempfile.mkdtemp()
 
+uploadinfo = []
+
 for jpeg in toupload:
     print jpeg
+    os.system('feh --full-screen --draw-filename "%s"' % jpeg)
     while True:
         name = userinput("Name:")
         desc = userinput("Description:")
         tags = userinput("Tags:")
+
+        if name.strip() == '.' and desc.strip() == '.' and tags.strip() == '.':
+            print "skipping"
+            print
+            break
 
         print name
         print
@@ -70,46 +78,50 @@ for jpeg in toupload:
             if len(yn) > 0 and (yn[0] is 'y' or yn[0] is 'n'):
                 good = True
         if yn[0] == 'y':
-            print "...uploading",jpeg,
-            sys.stdout.flush()
-
-            try:
-                tmpjpeg = os.path.join(tmpdir,os.path.split(jpeg)[-1])
-                shutil.copyfile(jpeg, tmpjpeg)
-                md = pyexiv2.ImageMetadata(tmpjpeg)
-                md.read()
-
-                md['Exif.Image.Copyright'] = 'mike warren'
-                for k in ['Exif.Photo.DateTimeDigitized',
-                          'Exif.Photo.DateTimeOriginal',
-                          'Exif.Image.Make',
-                          'Exif.Image.Model',
-                          'Exif.Image.DateTime',
-                          'Exif.Manufacturer',
-                          'Exif.Model'
-                          ]:
-                    try:
-                        del md[k]
-                    except:
-                        print "already gone:",k
-
-                md.write()
-                photoid = mikeapi.upload(tmpjpeg,
-                                         title=name,
-                                         description=desc,
-                                         tags=tags,
-                                         isPublic=True,
-                                         isFriend=False,
-                                         isFamily=False)
-                os.unlink(tmpjpeg)
-                print "done; id=",photoid
-            except:
-                print "ERROR"
-                raise
-
+            uploadinfo.append( (jpeg, name, desc, tags) )
             break
 
-        else:
-            print "trying again"
+
+for (jpeg, name, desc, tags) in uploadinfo:
+    print "Uploading:",jpeg
+
+    try:
+        tmpjpeg = os.path.join(tmpdir,os.path.split(jpeg)[-1])
+        shutil.copyfile(jpeg, tmpjpeg)
+        md = pyexiv2.ImageMetadata(tmpjpeg)
+        md.read()
+
+        md['Exif.Image.Copyright'] = 'mike warren'
+        print '   removing:',
+        for k in ['Exif.Photo.DateTimeDigitized',
+                  'Exif.Photo.DateTimeOriginal',
+                  'Exif.Image.Make',
+                  'Exif.Image.Model',
+                  'Exif.Image.DateTime'
+                  ]:
+            print k,
+            try:
+                del md[k]
+            except:
+                print "(not present)",
+            print ",",
+
+        print
+        print name
+        print description
+        print tags
+        
+        md.write()
+        photoid = mikeapi.upload(tmpjpeg,
+                                 title=name,
+                                 description=desc,
+                                 tags=tags,
+                                 isPublic=True,
+                                 isFriend=False,
+                                 isFamily=False)
+        os.unlink(tmpjpeg)
+        print "done; id=",photoid
+    except:
+        print "ERROR"
 
 shutil.rmtree(tmpdir)
